60274a71ea916d54362974a41b7ccd8b664bdb93
Luidrocha <luidrocha@yahoo.com.br>
2023-05-07 11:45:35 -0300

    Adicionado tabcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaClienteComponent } from './lista-cliente/lista-cliente.component';
 import { PageDefaltComponent } from './page-defalt/page-defalt.component';
+import { DataTablecliComponent } from './data-tablecli/data-tablecli.component';
 
 
 const routes: Routes = [
   {path:'cliente',component: ListaClienteComponent},
   {path:'home',component: PageDefaltComponent},
+  {path: 'about',component: DataTablecliComponent},
   {path: ' ', redirectTo:'/PageDefaltComponent', pathMatch:'full'},
 ];
 
